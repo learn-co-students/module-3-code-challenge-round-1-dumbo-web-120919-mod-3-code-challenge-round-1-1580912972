6462182a4947998e7bda56e41b662843dacef227
itslizkim <elizabeth.kim93@gmail.com>
2020-02-06 14:58:28 -0500

    fixed errors, fulfilled deliverables

--- src/index.js
+++ src/index.js
@@ -1,45 +1,51 @@
 /** intial render **/
-function getBeers(){
     fetch (`http://localhost:3000/beers`)
     .then (r => r.json())
     .then (beers => renderBeers(beers))
-}
 
-getBeers();
 /** Getting beer name for side bar */
 function renderBeers(beers){
-    let beerListContainer = document.querySelector('#list-group')
-    let beerUl= document.createElement('ul')
-    beerUl.className = 'list-group'
-    beerListContainer.append(beerUl)
+    let beerList = document.querySelector('#list-group')
+    
     
     beers.forEach(beer => {
         let beerLi = document.createElement('li')
-        beerLi.innerHTML= `<li class="list-group-item">${beer.name}</li>`
-        beerUl.append(beerLi)
+        beerLi.className = 'list-group-item'
+        beerLi.innerHTML= beer.name
 
-        beerLi.addEventListener('click', () => { //when clicking on beer name, render details
-            let beerDetailsContainer = document.querySelector('#beer-detail')
-            let beerInfo = document.createElement('div')
-            beerInfo.innerHTML = `
+        beerList.append(beerLi)
+
+        beerLi.addEventListener('click', () => {
+             //when clicking on beer name, render one beer details
+            let beerDetails = document.querySelector('#beer-detail')
+            beerDetails.innerHTML = `
             <h1>${beer.name}</h1>
             <img src=${beer.image_url}>
             <h3>${beer.tagline}</h3>
-            <textarea>${beer.description}</textarea>
+            <textarea id='description'>${beer.description}</textarea>
             <button type='submit' id="edit-beer" class="btn btn-info">
                 Save
             </button>
             `
-            beerDetailsContainer.append(beerInfo)
+            let button = beerDetails.querySelector("button")
 
-        }) 
+            button.addEventListener('click', () => {
+                let updatedDesc = beerDetails.querySelector('#description').value
+                fetch(`http://localhost:3000/beers/${beer.id}`,{
+                    method: "PATCH",
+                    helpers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                    },
+                    body: JSON.stringify({description: updatedDesc})
+                })
+                .then(r => r.json())
+                .then(beer.description = updatedDesc); 
 
-        
+            })
 
-        
-
-
-        
+                
+        })
 
     })
     
